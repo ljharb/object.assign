cfee6b7c2f028cb92e50d8f7933500d705182c57
Jordan Harband <ljharb@gmail.com>
2015-05-18 13:07:21 -0700

    Make sure `.shim` is non-enumerable.

--- index.js
+++ index.js
@@ -31,27 +31,29 @@ var assignShim = function assign(target, source1) {
 	return objTarget;
 };
 
-assignShim.shim = function shimObjectAssign() {
-	var assignHasPendingExceptions = function () {
-		if (!Object.assign || !Object.preventExtensions) {
-			return false;
-		}
-		// Firefox 37 still has "pending exception" logic in its Object.assign implementation,
-		// which is 72% slower than our shim, and Firefox 40's native implementation.
-		var thrower = Object.preventExtensions({ 1: 2 });
-		try {
-			Object.assign(thrower, 'xy');
-		} catch (e) {
-			return thrower[1] === 'y';
-		}
-	};
-	defineProperties(
-		Object,
-		{ assign: assignShim },
-		{ assign: assignHasPendingExceptions }
-	);
-	return Object.assign || assignShim;
-};
+defineProperties(assignShim, {
+	shim: function shimObjectAssign() {
+		var assignHasPendingExceptions = function () {
+			if (!Object.assign || !Object.preventExtensions) {
+				return false;
+			}
+			// Firefox 37 still has "pending exception" logic in its Object.assign implementation,
+			// which is 72% slower than our shim, and Firefox 40's native implementation.
+			var thrower = Object.preventExtensions({ 1: 2 });
+			try {
+				Object.assign(thrower, 'xy');
+			} catch (e) {
+				return thrower[1] === 'y';
+			}
+		};
+		defineProperties(
+			Object,
+			{ assign: assignShim },
+			{ assign: assignHasPendingExceptions }
+		);
+		return Object.assign || assignShim;
+	}
+});
 
 module.exports = assignShim;
 
