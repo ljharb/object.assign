9a75cf7e57148fe8e156256b16198b81e0e969dd
Jordan Harband <ljharb@gmail.com>
2015-10-24 09:54:05 -0700

    Enabling these `Symbol` tests.
    Per https://github.com/zloirock/core-js/issues/123#issuecomment-150668037

--- hasSymbols.js
+++ hasSymbols.js
@@ -11,9 +11,8 @@ module.exports = function hasSymbols() {
 	var symObj = Object(sym);
 	if (typeof sym === 'string') { return false; }
 
-	// temp disabled per https://github.com/zloirock/core-js/issues/123
-	// if (Object.prototype.toString.call(sym) !== '[object Symbol]') { return false; }
-	// if (Object.prototype.toString.call(symObj) !== '[object Symbol]') { return false; }
+	if (Object.prototype.toString.call(sym) !== '[object Symbol]') { return false; }
+	if (Object.prototype.toString.call(symObj) !== '[object Symbol]') { return false; }
 
 	// temp disabled per https://github.com/ljharb/object.assign/issues/17
 	// if (sym instanceof Symbol) { return false; }
